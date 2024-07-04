c4ecf2287ac23d1644ccb67d993343f4dd95d75f
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2024-07-05 02:08:48 +0430

    create component: icon button
    Des:
    create icon button component

---
+++ src/components/buttonComponent/iconBtn.js
@@ -0,0 +1,18 @@
+import domGenerator from "dom-generator";
+
+/**
+ * create icon button component
+ * first destructure object and get (src) and (alt) then use these data in component
+ * @param {object} param0 icon data in json file (src, alt)
+ * @returns icon button component
+ */
+export function iconBtn({ src, alt }) {
+    return domGenerator({
+        tag: "button",
+        children: [{
+            tag: "img",
+            attributes: { class: "w-7" },
+            properties: { src: src, alt: alt }
+        }]
+    })
+}
