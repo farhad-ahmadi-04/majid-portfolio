ec639953c2c7843efd1347ad3b8b648a1c1b8688
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2024-07-05 03:56:05 +0430

    update & refactor: footer
    Des:
    create another item of footer & using grid instead of flex for style

--- src/sectionCreator/footer/footer.js
+++ src/sectionCreator/footer/footer.js
@@ -1,5 +1,6 @@
 import domGenerator from "dom-generator";
 import icons from "../../datas/icons.json"
+import { iconBtn } from "../../components/buttonComponent/iconBtn";
 
 /**
  * craete footer component by using domGenerator
@@ -11,41 +12,59 @@ export function footer() {
         attributes: { class: "bg-blue-dark" },
         children: [{
             tag: "div",
-            attributes: { class: "container grid grid-cols-2 justify-items-center md:flex-wrap md:grid-cols-1" },
+            attributes: { class: "container grid grid-cols-3 justify-items-center md:grid-cols-1 md:grid-rows-2" },
             children: [{
                 tag: "div",
-                attributes: { class: "container-item flex justify-items-center items-center" },
+                attributes: { class: "container-item flex flex-col justify-between items-center gap-4 w-full" },
+                children: [{
+                    tag: "img",
+                    attributes: { class: "xsm:hidden w-32 h-40" },
+                    properties: { src: icons.footer.avatar }
+                }, {
+                    tag: "h1",
+                    attributes: { class: "text-4xl font-bold text-center text-Beige" },
+                    properties: { textContent: "Giving Thanks" }
+                }, {
+                    tag: "div",
+                    attributes: { class: "flex flex-col justify-center text-Beige-dark gap-1" },
+                    children: [{
+                        tag: "p",
+                        attributes: { class: "text-center text-2xl" },
+                        properties: { textContent: "Get in touch:" }
+                    }, {
+                        tag: "p",
+                        attributes: { class: "text-center text-xl" },
+                        properties: { textContent: "majidsheshbelooki@gmail.com" }
+                    }]
+                }],
+            }, {
+                tag: "div",
+                attributes: { class: "container-item  w-full flex flex-col " },
                 children: [{
                     tag: "div",
-                    attributes: { class: "item-first flex flex-col justify-center items-center gap-3" },
+                    attributes: { class: "xsm:hidden w-60 h-56 pt-5" },
                     children: [{
                         tag: "img",
-                        attributes: { class: "xsm:hidden w-32 h-40" },
-                        properties: { src: icons.footer.avatar }
-                    }, {
-                        tag: "h1",
-                        attributes: { class: "text-4xl font-bold text-center text-Beige" },
-                        properties: { textContent: "Giving Thanks" }
-                    }, {
-                        tag: "div",
-                        attributes: { class: "flex flex-col justify-center text-Beige-dark gap-1" },
-                        children: [{
-                            tag: "p",
-                            attributes: { class: "text-center text-2xl" },
-                            properties: { textContent: "Get in touch:" }
-                        }, {
-                            tag: "p",
-                            attributes: { class: "text-center text-xl" },
-                            properties: { textContent: "majidsheshbelooki@gmail.com" }
-                        }]
+                        attributes: { class: "w-11/12" },
+                        properties: { src: icons.footer.arrow }
                     }]
                 }, {
                     tag: "div",
-                    attributes: { class: "item-second" },
-                }]
+                    attributes: { class: "flex justify-center w-full items-center xsm:flex-col xsm:justify-center" },
+                    children: [{
+                        tag: iconBtn(icons.icons.WhatsApp),
+                    }, {
+                        tag: iconBtn(icons.icons.Instagram),
+                    }, {
+                        tag: iconBtn(icons.icons.LinkedIn),
+                    }, {
+                        tag: iconBtn(icons.icons.Telegram),
+                    }]
+                }],
             }, {
                 tag: "div",
-                attributes: { class: "container-item" }
+                attributes: { class: "container-item  w-full" },
+
             }]
 
         }]
