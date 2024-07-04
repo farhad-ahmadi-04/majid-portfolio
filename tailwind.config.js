0a35525f2c76a0a112400b5065c89dc1b33f4b7a
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2024-07-05 03:54:39 +0430

    update tailwind config file
    Des:
    update screens sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,7 +28,8 @@ export default {
       sm: { 'max': '640px' },
       md: { 'max': '768px' },
       lg: { 'max': '1024px' },
-      xl: { 'max': '1280px' }
+      xl: { 'max': '1280px' },
+      "2xl": { 'max': '1536px' },
     },
     plugins: [],
   }
