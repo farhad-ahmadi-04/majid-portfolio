9c157a29de0b24fe3a07f067ea646dee6d92c79b
mehrdadj2005 <mehrdadjokari13@gmail.com>
2024-07-09 13:48:46 +0330

    # add hero section in dom

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./index.css";
 import render from "./components/render.js";
 import { footer } from "./sectionCreator/footer/footer.js";
+import hero from "./sectionCreator/hero/hero";
 import header from "./sectionCreator/header/header.js";
 
 let app = document.querySelector("#app")
@@ -10,6 +11,7 @@ let app = document.querySelector("#app")
  */
 function loadContent() {
     render(app, header())
+    render(app, hero())
     render(app, footer())
 }
 loadContent()
