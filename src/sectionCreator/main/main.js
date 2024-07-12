8030a66681c156f8987507e80a969a09933e6a01
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2024-07-12 20:27:47 +0430

    update: main section
    Des:
    get render from aboutMe section in main section and change background color of main

--- src/sectionCreator/main/main.js
+++ src/sectionCreator/main/main.js
@@ -1,11 +1,15 @@
 import domGenerator from "dom-generator";
 import hero from "../hero/hero";
+import about from "./aboutMe/about";
 
 export default function main() {
     return domGenerator({
         tag: "main",
+        attributes: { class: "bg-Beige" },
         children: [{
             tag: hero()
+        }, {
+            tag: about()
         }]
     })
 }
