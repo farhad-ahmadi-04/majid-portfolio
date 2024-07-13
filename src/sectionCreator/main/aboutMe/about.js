c55237a28e065764c243db86b540f1d1255a2d10
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2024-07-13 15:45:53 +0430

    update: font style
    Des:
    set new font size for mobile

--- src/sectionCreator/main/aboutMe/about.js
+++ src/sectionCreator/main/aboutMe/about.js
@@ -25,11 +25,11 @@ export default function about() {
                 attributes: { class: "about-texts" },
                 children: [{
                     tag: "h1",
-                    attributes: { class: "text-center text-blue-dark text-5xl font-bold" },
+                    attributes: { class: "text-center text-blue-dark text-5xl xsm:text-3xl font-bold" },
                     properties: { textContent: "Hellow" }
                 }, {
                     tag: "p",
-                    attributes: { class: "text-center text-blue-dark text-2xl" },
+                    attributes: { class: "text-center text-blue-dark text-xl xsm:text-lg" },
                     properties: { textContent: "I`am majid sheshbelooki jjdksd djdjsbbyuer ivjfdocnoifvn duvjjf iiijfdwokp rokvdsv virov friojvii jif thsvn dmkmkfmk vmkmvmmw f em ‘UserInterFace Designer dkfncodkf odso ffmkd fne nffnfnfdknfkdkf fiojf fjjfi jfjeof fjijijfiwo f ffj je f efiof f ieolflkvnkdfnjgpe3t n mfvkfmkvmor ggorigoorgvmfdkvmkae[qepfmd;vmfmg pfmpaf  plefpw,fpf,mpfm " }
                 }]
             }]
