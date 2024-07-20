60d4bc35393e7f397fac9284e728f91b410e1d30
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2024-07-20 18:07:03 +0430

    Update cardSkill.js
    Des:
    add extra class to component

--- src/components/cardSkills.js
+++ src/components/cardSkills.js
@@ -1,29 +1,28 @@
 import domGenerator from "dom-generator"
 
-export default function cardSkills(text, bgColor, rotate, minusRotate) {
+export default function cardSkills(text, bgColor, max, min, extraClass = "") {
     return domGenerator(
         {
             tag: "div",
             attributes: {
-                class: `${bgColor} w-44 h-44 rotate-${rotate = 0} 
-                flex items-center justify-center
-                `,
-                style: `transform: rotate(${minusRotate = 0});`
+                class: `${bgColor} w-28 h-28 p-2
+                flex items-center justify-center md:rotate-0 ${extraClass}`,
+                style: `transform: rotate(${Math.floor(Math.random() * (max - min + 1)) + min}deg);`
             },
             children: [
                 {
                     tag: "div",
-                    attributes: { class: `border w-40 h-40 flex items-center justify-center` },
+                    attributes: { class: `border-2 border-blue-light w-full h-full flex items-center justify-center` },
                     children: {
                         tag: "span",
-                        attributes: { class: "text-sm" },
+                        attributes: { class: "text-sm font-semibold text-center" },
                         properties: { textContent: text }
                     }
                 },
                 {
                     tag: "img",
                     attributes: {
-                        class: "absolute bottom-40 left-40",
+                        class: "absolute -top-2.5 -right-2.5",
                         src: "/images/Paperclip.svg",
                         alt: "Paperclip"
                     }
