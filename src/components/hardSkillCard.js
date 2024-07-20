7da5bb83d505242becffdf6dcad31d74d9b56f41
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2024-07-20 14:57:41 +0430

    Update hardSkillCrad.js
    Des:
    adding condition to set class.
    update style.

--- src/components/hardSkillCard.js
+++ src/components/hardSkillCard.js
@@ -3,15 +3,16 @@ import domGenerator from "dom-generator"
 /**
  * create card component for hard skill by don generator and adding tailwind styles 
  * @param {string} skill -  hard skill  
+ * @param {string} position -  set position of element  
  * @returns - card component (element)😎
  */
-export default function hardSkillCard(skill) {
+export default function hardSkillCard(skill, position = "self-start") {
     return domGenerator({
         tag: "div",
-        attributes: { class: "px-4 py-2 rounded-lg bg-Beige-Medium inline-block" },
+        attributes: { class: `px-4 py-2 rounded-lg bg-Beige-Medium w-fit text-center ${position.toLocaleLowerCase() === "right" ? 'self-end' : position}` },
         children: [{
             tag: "span",
-            attributes: { class: "text-2xl md:text-base font-bold text-cards-text text-center" },
+            attributes: { class: "text-2xl md:text-base font-bold text-cards-text " },
             properties: { textContent: skill }
         }]
     })
