0295e9891dd4caca8013d521d3efa6e39a5fbaf1
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2024-07-21 16:50:09 +0430

    Update cardSkill.js
    Des:
    add width to card component in small media

--- src/components/cardSkills.js
+++ src/components/cardSkills.js
@@ -5,7 +5,7 @@ export default function cardSkills(text, bgColor, max, min, extraClass = "") {
         {
             tag: "div",
             attributes: {
-                class: `${bgColor} w-28 h-28 p-2
+                class: `${bgColor} w-28 h-28 sm:w-24 sm:h-24 p-2 md:p-0.5
                 flex items-center justify-center md:rotate-0 ${extraClass}`,
                 style: `transform: rotate(${Math.floor(Math.random() * (max - min + 1)) + min}deg);`
             },
@@ -15,7 +15,7 @@ export default function cardSkills(text, bgColor, max, min, extraClass = "") {
                     attributes: { class: `border-2 border-blue-light w-full h-full flex items-center justify-center` },
                     children: {
                         tag: "span",
-                        attributes: { class: "text-sm font-semibold text-center" },
+                        attributes: { class: "text-sm sm:text-xs font-semibold text-center" },
                         properties: { textContent: text }
                     }
                 },
