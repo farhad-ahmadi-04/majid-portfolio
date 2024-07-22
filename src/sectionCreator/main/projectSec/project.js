06b63ca5b5ddcd20f415848d30bddd1e8db45ca9
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2024-07-22 17:39:18 +0430

    Update project.js
    Des:
    add code to create section with out logic just UI

--- src/sectionCreator/main/projectSec/project.js
+++ src/sectionCreator/main/projectSec/project.js
@@ -1,4 +1,7 @@
-import domGenerator from 'dom-generator';
+import domGenerator from "dom-generator";
+import { iconBtn } from "../../../components/buttonComponent/iconBtn";
+import images from "../../../datas/images.json"
+console.log(images.arrows.arrowLHero);
 
 /**
  * create element (section) with dom-generator
@@ -6,6 +9,52 @@ import domGenerator from 'dom-generator';
  */
 export function project() {
     return domGenerator({
+        tag: 'section',
+        attributes: { class: 'p-5 bg-blue-pattern' },
+        children: [{
+            tag: 'h1',
+            attributes: { class: "text-3xl font-bold text-Beige text-center mb-4 hidden md:block" },
+            properties: { textContent: 'Projects' }
+        }, {
+            tag: 'div',
+            attributes: { class: 'container' },
+            children: [{
+                tag: 'h1',
+                attributes: { class: "text-3xl font-bold text-Beige text-center mb-4 md:hidden" },
+                properties: { textContent: 'Projects' }
+            }, {
+                tag: "div",
+                attributes: { class: "flex justify-between items-center" },
+                children: [
+                    {
+                        tag: iconBtn({
+                            src: images.arrows["arrow-l-portfolio"],
+                            alt: "arrow icon left"
+                        }, "md:hidden"),
+                    }, {
+                        tag: 'div',
+                        attributes: { class: "flex justify-center items-center" },
+                        children: [{
+                            tag: 'img',
+                            properties: { src: images.images.projects.p1, alt: "project" }
+                        }, {
+                            tag: 'img',
+                            properties: { src: images.images.projects.p2, alt: "project" }
+                        }, {
+                            tag: 'img',
+                            properties: { src: images.images.projects.p3, alt: "project" }
+                        }, {
+                            tag: 'img',
+                            properties: { src: images.images.projects.p4, alt: "project" }
+                        }]
 
+                    }, {
+                        tag: iconBtn({
+                            src: images.arrows["arrow-r-portfolio"],
+                            alt: "arrow icon left"
+                        }, "md:hidden")
+                    }]
+            }]
+        }]
     })
 }
