4d168edcb02b9abd071e98c4b5cc29d0265df330
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2024-07-22 17:38:20 +0430

    Update icon button component
    Des:
    add extra class to have more class if we want

--- src/components/buttonComponent/iconBtn.js
+++ src/components/buttonComponent/iconBtn.js
@@ -4,12 +4,13 @@ import domGenerator from "dom-generator";
  * create icon button component
  * first destructure object and get (src) and (alt) then use these data in component
  * @param {object} param0 icon data in json file (src, alt)
+ * @param {string} extraClass - extra class for add class to component
  * @returns icon button component
  */
-export function iconBtn({ src, alt }) {
+export function iconBtn({ src, alt }, extraClass = "") {
     return domGenerator({
         tag: "button",
-        attributes: { class: "p-1" },
+        attributes: { class: `p-1 ${extraClass}` },
         children: [{
             tag: "img",
             attributes: { class: "w-7" },
