e2465aa57a848f7a92885fa70afb8d3c6c4d87ce
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2024-07-22 16:05:14 +0430

    create project.js
    Des:
    create js file for project section

---
+++ src/sectionCreator/main/projectSec/project.js
@@ -0,0 +1,11 @@
+import domGenerator from 'dom-generator';
+
+/**
+ * create element (section) with dom-generator
+ * @returns element
+ */
+export function project() {
+    return domGenerator({
+
+    })
+}
